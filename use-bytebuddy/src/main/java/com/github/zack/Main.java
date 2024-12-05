12789b569649c9444eb0620d48145628feeb7b86
zhouze <ze.zhou@whats-egg.com>
2024-12-05 07:39:19 +0000

    c all

---
+++ use-bytebuddy/src/main/java/com/github/zack/Main.java
@@ -0,0 +1,11 @@
+package com.github.zack;
+
+/**
+ * @author zhouze
+ * @date 2024/4/17
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
